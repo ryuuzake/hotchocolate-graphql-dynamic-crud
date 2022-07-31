61c9ca5e5589681d4db774b6f139623f36d3a425
Riza Nafis <riza.nafis@gmail.com>
2022-07-31 10:34:03 +0700

    able to generate get query using reflection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,49 @@
+using GraphQL.DynamicCRUD.Data;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>();
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType(descriptor =>
+    {
+        // Loop all DBSet
+        var dbSets = typeof(AppDbContext)
+            .GetRuntimeFields();
+
+        foreach (var dbSet in dbSets)
+        {
+            var name = dbSet.Name.Split('>')[0].Replace("<", "");
+            var type = dbSet.FieldType.GetGenericArguments()[0];
+
+            descriptor
+                .Field($"get{name}")
+                .Type(typeof(IQueryable<>).MakeGenericType(type))
+                //.UsePaging(
+                //    entityType: type,
+                //    options: new PagingOptions
+                //    {
+                //        IncludeTotalCount = true
+                //    })
+                //.UseProjection()
+                //.UseFiltering()
+                //.UseSorting()
+                .Resolve(r =>
+                    {
+                        var resolver = dbSet.GetValue(r.Service<AppDbContext>());
+
+                        return resolver;
+                    });
+        }
+    })
+    .AddFiltering()
+    .AddSorting()
+    .AddProjections();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGraphQL();
 
 app.Run();
